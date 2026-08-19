26922381633e152a448a86cd8322712007efbf3c
Cornelius Putzler-Marci <10773394+cornz@users.noreply.github.com>
2026-08-19 11:13:28 +0200

    Update TrimWM to 0.4.0

--- Casks/trimwm.rb
+++ Casks/trimwm.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 cask "trimwm" do
-  version "0.3.1"
-  sha256 "69477a8018e84c6b8847d5aedf2bd996310821fbdbe5a3d9458396e6aec1c5f8"
+  version "0.4.0"
+  sha256 "23a40450632dec403ca62fe13c7860e8a6bd11a095982783629f4a41243e5721"
 
   url "https://github.com/cornz/TrimWM/releases/download/v#{version}/TrimWM-#{version}.zip",
       verified: "github.com/cornz/TrimWM/"
